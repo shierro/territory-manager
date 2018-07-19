accdcd77d5ae3c56a5e2fdaaccea605ee0a4c9ba
shierro <theodoro.pelingan@gmail.com>
2018-07-19 12:02:36 +0800

    fixed cc issues related to mapPage index file & configureStore

---
+++ app/containers/MapPage/markerIcon.js
@@ -0,0 +1,10 @@
+import { icon } from 'leaflet';
+
+export default icon({
+  iconUrl:
+    'https://raw.githubusercontent.com/shierro/leaflet-color-markers/master/img/marker-icon-red.png',
+  iconSize: [25, 41], // size of the icon
+  shadowSize: [50, 64], // size of the shadow
+  iconAnchor: [12, 41], // point of the icon which will correspond to marker's location
+  shadowAnchor: [4, 62], // the same for the shadow
+});
