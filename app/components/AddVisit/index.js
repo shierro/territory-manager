a5f899d67d90a8a69917a72faa1fcccb012a5afb
shierro <theodoro.pelingan@gmail.com>
2018-08-25 16:54:48 +0800

    :sparkles: when adding a visit on popup, person details are hidden to give space for mobile view :hammer: LeftDrawer styles to a separate file

--- app/components/AddVisit/index.js
+++ app/components/AddVisit/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import IconButton from '@material-ui/core/IconButton';
 import PropTypes from 'prop-types';
-import NotFound from '@material-ui/icons/VoiceOverOff';
+import VoiceOverOff from '@material-ui/icons/VoiceOverOff';
 import CheckCircle from '@material-ui/icons/CheckCircle';
 import Save from '@material-ui/icons/Save';
 import Cancel from '@material-ui/icons/Cancel';
 import Badge from '@material-ui/core/Badge';
 import TextField from '@material-ui/core/TextField';
+import Tooltip from '@material-ui/core/Tooltip';
 
 import styles from './styles';
 
@@ -15,19 +16,23 @@ class AddVisit extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      addingVisit: false,
       note: '',
       visitState: '',
     };
     this.saveVisit = this.saveVisit.bind(this);
+    this.startVisitAdd = this.startVisitAdd.bind(this);
+  }
+  startVisitAdd(name) {
+    this.setState({ visitState: name });
+    if (!this.props.addingVisit) {
+      this.props.toggleAddingVisit();
+    }
   }
   saveVisit() {
     const { visitState, note } = this.state;
-    this.props.saveVisit({
-      note,
-      found: visitState === 'found',
-    });
-    this.setState({ addingVisit: false });
+    const found = visitState === 'Found';
+    this.props.saveVisit({ note, found });
+    this.setState({ note: '', visitState: '' });
   }
   renderBadge(content, name, icon) {
     const { classes } = this.props;
@@ -39,56 +44,74 @@ class AddVisit extends React.Component {
       </Badge>
     );
   }
+  renderHeaderButtons(visitTypes, classes, count) {
+    return visitTypes.map(({ name, icon }, key) => (
+      <div className={classes.column} key={`visit-icon-${key + 1}`}>
+        <Tooltip title={`Add visit - ${name}`} placement="top">
+          <IconButton
+            aria-label={name}
+            onClick={() => this.startVisitAdd(name)}
+          >
+            {this.renderBadge(count[name], name, icon)}
+          </IconButton>
+        </Tooltip>
+      </div>
+    ));
+  }
   renderHeader(classes, person) {
     const visitTypes = [
-      { name: 'found', icon: <CheckCircle color="secondary" /> },
-      { name: 'notFound', icon: <NotFound color="error" /> },
+      { name: 'Found', icon: <CheckCircle color="secondary" /> },
+      { name: 'NotFound', icon: <VoiceOverOff color="error" /> },
     ];
     const count = {
-      found: person.visits.filter(prsn => prsn.found).length,
-      notFound: person.visits.filter(prsn => !prsn.found).length,
+      Found: person.visits.filter(prsn => prsn.found).length,
+      NotFound: person.visits.filter(prsn => !prsn.found).length,
     };
     return (
       <div>
-        <h4 className={classes.visitLabel}>Visits: ({person.visits.length})</h4>
-        {visitTypes.map(({ name, icon }, key) => (
-          <div className={classes.column} key={`visit-icon-${key + 1}`}>
-            <IconButton
-              aria-label={name}
-              onClick={() =>
-                this.setState({ visitState: name, addingVisit: true })
-              }
-            >
-              {this.renderBadge(count[name], name, icon)}
-            </IconButton>
-          </div>
-        ))}
+        <h3 className={classes.visitLabel}>
+          Total visits: ({person.visits.length})
+        </h3>
+        {this.props.addingVisit && (
+          <h4>Add new visit ({this.state.visitState})</h4>
+        )}
+        {this.renderHeaderButtons(visitTypes, classes, count)}
       </div>
     );
   }
   renderActionButtons() {
+    const saveDisabled = !this.state.note;
+    const saveColor = saveDisabled ? 'disabled' : 'secondary';
     return (
       <div>
-        <IconButton aria-label="Save" onClick={this.saveVisit}>
-          <Save color="secondary" />
-        </IconButton>
         <IconButton
-          aria-label="Cancel"
-          onClick={() => this.setState({ addingVisit: false })}
+          aria-label="Save"
+          disabled={saveDisabled}
+          onClick={this.saveVisit}
         >
-          <Cancel color="error" />
+          <Save color={saveColor} />
         </IconButton>
+        <Tooltip title="Cancel" placement="top">
+          <IconButton
+            aria-label="Cancel"
+            onClick={this.props.toggleAddingVisit}
+          >
+            <Cancel color="error" />
+          </IconButton>
+        </Tooltip>
       </div>
     );
   }
   render() {
-    const { classes, person } = this.props;
-    const { addingVisit, note } = this.state;
+    const { classes, person, addingVisit } = this.props;
+    const { note } = this.state;
     return (
       <div className={classes.container}>
         {this.renderHeader(classes, person)}
         <div style={{ display: addingVisit ? 'inline-block' : 'none' }}>
           <TextField
+            className={classes.noteField}
+            placeholder="Add some notes.."
             value={note}
             onChange={({ target: { value } }) => this.setState({ note: value })}
             InputLabelProps={{ shrink: true }}
@@ -108,6 +131,8 @@ AddVisit.propTypes = {
   classes: PropTypes.object.isRequired,
   person: PropTypes.object.isRequired,
   saveVisit: PropTypes.func.isRequired,
+  addingVisit: PropTypes.bool.isRequired,
+  toggleAddingVisit: PropTypes.func.isRequired,
 };
 
 export default withStyles(styles)(AddVisit);
